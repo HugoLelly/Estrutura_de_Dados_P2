3dd810868f8272e9091248d826cc35f41e3f10b0
Hugo Lelly de Lima Marinho <lellysoueu.hg@gmail.com>
2023-05-31 23:01:45 -0300

    commit

--- Cod_01/Cadastro_Animal.py
+++ Cod_01/Cadastro_Animal.py
@@ -27,48 +27,60 @@ def cadastro_animal():
             particularidades = str(input(f"\n\tInforme as particularidades do animal: "))
             cad_particularidades.append(particularidades)
             sleep(3)
-            os.system('clear') or None
+            os.system('cls') or None
+            print(f"\n\tANIMAL CADASTRADO")
             print(f"\n\tEspécie: {cad_especie} \n\tRaça: {cad_raca} \n\tIdade: {cad_idade} \n\tCor: {cad_cor} \n\tPorte: {cad_porte} \n\tParticularidades: {cad_particularidades}")
-            sleep(3)
-            os.system('clear') or None
+            sleep(5)
+            os.system('cls') or None
         elif cad_an == "N" or cad_an == "n":
-            print(f"\n\tSaindo do Cadastro!\n\taguarde...")
+            print(f"\n\tSaindo do Cadastro!\n\tAguarde...")
+
             sleep(3)
-            os.system('clear') or None
             menu()
         else:
-            print(f"\n\tOpção Inválida!\n\taguarde...")
+            print(f"\n\tOpção Inválida!\n\tAguarde...")
             sleep(3)
-            os.system('clear') or None
+            os.system('cls') or None
 
 def cadastro_candidatos():
+    os.system('cls') or None
     print("\n\tCADASTRO DOS CANDIDATOS PARA ADOÇÃO")
     sleep(3)
-    os.system('clear') or None
     menu()
 
 def consulta():
+    os.system('cls') or None
     print("\n\tCONSULTA DOS ANIMAIS PARA ADOÇÃO")
     sleep(3)
-    os.system('clear') or None
     menu()
 
 def menu():
+    os.system('cls') or None
     print(f"\n\tBEM VINDO AO SISTEMA DE ADOÇÃO DE ANIMAIS\n\tEscolha uma das opções abaixo:\n\t[1] Cadastrar Animal\n\t[2] Cadastrar Candidatos\n\t[3] Consultar Animais para Adoção\n\t[4] Sair do Sistema")
     opcao = int(input(f"\n\tInforme a opção desejada: "))
     if opcao == 1:
+        os.system('cls') or None
+        print("\n\tAbrindo o Cadastro Animal!\n\tAguarde...")
+        sleep(3)
         cadastro_animal()
     elif opcao == 2:
+        os.system('cls') or None
+        print("\n\tAbrindo o Cadastro de Candidatos!\n\tAguarde...")
+        sleep(3)
         cadastro_candidatos()
     elif opcao == 3:
+        os.system('cls') or None
+        print("\n\tAbrindo a Consulta dos Animais para Adoção!\n\tAguarde...")
+        sleep(3)
         consulta()
     elif opcao == 4:
-        print(f"\n\tSaindo do Sistema!\n\taguarde...")
+        os.system('cls') or None
+        print(f"\n\tSaindo do Sistema!\n\tAguarde...")
         sleep(3)
-        os.system('clear') or None
+        exit()
     else:
-        print(f"\n\tOpção Inválida!\n\taguarde...")
+        os.system('cls') or None
+        print(f"\n\tOpção Inválida!\n\tAguarde...")
         sleep(3)
-        os.system('clear') or None
 
 menu()
