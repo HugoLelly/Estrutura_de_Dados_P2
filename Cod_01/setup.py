d7bbe854a0c56c55d4043379cbbe98cb6019ac2a
Hugo Lelly de Lima Marinho <lellysoueu.hg@gmail.com>
2023-06-12 19:25:33 -0300

    commit

--- Cod_01/setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Sistema de Adoção de Animais",
-    version="1.0",
-    description="CURSO: ENGENHARIA DE SOFTWARE. DISCIPLINA: ESTRUTURA DE DADOS. PROFESSOR: MÁRCIO GARRIDO. TÍTULO: P2 - ENUNCIADO E REGRAS. TURMA: 2022.1 – TURMA A. MATRÍCULA: 202211182. ALUNO: HUGO LELY DE LIMA MARINHO.",
-    executables=[Executable("Cod_Completo.py")]
-)
--- Sistema_de_Adocao_TKInter/limparterminal.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Função para limpar o terminal
-def limpar_terminal():
-    os.system("cls" if os.name == "nt" else "clear")
